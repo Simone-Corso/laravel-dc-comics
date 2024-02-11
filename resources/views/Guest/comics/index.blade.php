02bf28fb3291e32d20a6b159b1786973d44a7609
Simone-Corso <scorso26@gmail.com>
2024-02-11 15:26:41 +0100

    ho modificato in modo corretto i button e messo centrali tutti e due

--- resources/views/Guest/comics/index.blade.php
+++ resources/views/Guest/comics/index.blade.php
@@ -7,19 +7,23 @@
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-3 mb-3">
                     @foreach ($comics as $comic)
                         <div class="col">
-                                <div class="card">
+                                <div class="card text-center">
                                     <img src="{{ $comic->thumb }}" alt="{{ $comic->title }} picture">
-                                    <form action="{{ route('guest.comics.show', $comic['id']) }}">
-                                <button type="submit" class="btn btn-primary">Visualizza la card</button>
-                            </form>
-                            <form action="{{ route('guest.comics.create') }}" method="GET">
-                                        @csrf
-                                        <button type="submit" class="btn btn-two">Crea La Tua Card</button>
                                     <div class="card-body">
                                         <p class="text-uppercase">
                                             {{ $comic->title }}
                                         </p>
                                     </div>
+                                    <div class="d-flex justify-content-center flex-column">
+                                    <div class="text-center">
+                                    <form action="{{ route('guest.comics.show', $comic['id']) }}">
+                                <button type="submit" class="btn btn-primary w-50 ">Visualizza la card</button>
+                            </form> 
+                            <form action="{{ route('guest.comics.create') }}" method="GET">
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary w-50 mt-1 mb-4">Crea La Tua Card</button>
+                                </div>
+                                </div>
                                 </div>
                             </a>
                         </div>
