67ae6d2daf504d754599db1f08bff0d738b8c390
Simone-Corso <scorso26@gmail.com>
2024-02-11 15:37:10 +0100

    cambiato colore

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar bg-primary d-flex justify-content-center align-items-center" data-bs-theme="dark">
+<nav class="navbar bg-primary d-flex justify-content-center align-items-center" data-bs-theme="#0D6EFD">
   <h1 class="title ">
     Comics
   </h1>
